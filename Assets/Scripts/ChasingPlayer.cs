bc6fb0f24c2ca8cc83d43582cd775806571dc8c3
Hanul-Oh <ohanoul13@gmail.com>
2022-11-25 22:06:02 +0900

    set player layer "Player"
    set player layer "Player"

---
+++ Assets/Scripts/ChasingPlayer.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChasingPlayer : MonoBehaviour
+{
+    public float moveSpeed;
+    Vector3 enemyMovement;
+    Rigidbody2D rb;
+
+    Transform playerPosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerPosition = TopViewCharacterBehavior.Instance.transform;
+        Debug.Log(TopViewCharacterBehavior.Instance.transform.position);
+        rb = this.GetComponent<Rigidbody2D>();
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(playerPosition);
+
+        Vector3 direction = playerPosition.position - transform.position;
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        //rb.rotation = angle;
+        direction.Normalize();
+        enemyMovement = direction;
+    }
+    private void FixedUpdate()
+    {
+        MoveCharacter(enemyMovement);
+    }
+    void MoveCharacter(Vector3 direction)
+    {
+        rb.MovePosition((Vector3)transform.position + (direction * moveSpeed * Time.deltaTime));
+    }
+}
