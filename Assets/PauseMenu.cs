fec26690f40404af24599247987816098c2be718
Hanul-Oh <ohanoul13@gmail.com>
2022-11-26 01:49:41 +0900

    Add ESC menu
    adding pause menu and button

--- Assets/PauseMenu.cs
+++ Assets/PauseMenu.cs
@@ -1,14 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class PauseMenu : MonoBehaviour
 {
     // Player input 속성은 Scene 내부에 단 하나만 있어야 한다?
     public static bool GameIsPaused = false;
     public GameObject pauseMenuCanvas;
+    public Button returnGame, exitGame;
+
+    void Start()
+    {
+        returnGame.onClick.AddListener(ResumeBtn);
+        exitGame.onClick.AddListener(ToTitle);
+    }
     void Update()
     {
+
         if (Input.GetKeyDown(KeyCode.Escape))
         {
             if (GameIsPaused)
@@ -27,6 +35,7 @@ public class PauseMenu : MonoBehaviour
         pauseMenuCanvas.SetActive(false);
         Time.timeScale = 1.0f;
         GameIsPaused = false;
+        Debug.Log("Resumed");
     }
 
     public void Pause()
@@ -34,16 +43,18 @@ public class PauseMenu : MonoBehaviour
         pauseMenuCanvas.SetActive(true);
         Time.timeScale = 0f;
         GameIsPaused = true;
+        Debug.Log("Paused");
     }
     // 돌아가기 버튼 
     public void ResumeBtn()
     {
+        Debug.Log("Resume btn");
         Resume();
     }
     // 타이틀로 가기 버튼 클릭 
     public void ToTitle()
     {
-        Debug.Log("to title");
+        Debug.Log("ToTitle btn");
         //Time.timeScale = 1f;
         //SceneManager.LoadScene("MainMenu");
     }
